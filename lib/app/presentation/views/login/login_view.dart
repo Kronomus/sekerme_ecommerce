23bb12c244ab8b5638433ae3e6bcb464cf4cd6f5
JuanEstebanHernandezRamirez <jehernandez554@soy.sena.edu.co>
2023-08-01 16:30:24 -0500

    se agrego la interfaz del login_view.dart y configuro las dependencias supabase_flutter 1.10.11 flutter_dotenv 5.1.0 ademas se actualizo el build.gradle ala version 19

--- lib/app/presentation/views/login/login_view.dart
+++ lib/app/presentation/views/login/login_view.dart
@@ -1,11 +1,18 @@
+
+import 'package:Our_School/app/presentation/views/forgot_password/forgot_%20password_view.dart';
+import 'package:Our_School/app/presentation/widgets/form_text_field.dart';
+import 'package:Our_School/app/presentation/widgets/my_button_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 
 class LoginView extends StatelessWidget {
 
   static const String name = 'login_view';
 
   const LoginView({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +23,62 @@ class LoginView extends StatelessWidget {
             width: double.infinity,
             padding: const EdgeInsets.all(8.0),
             child: Column(
+              crossAxisAlignment : CrossAxisAlignment.center,
               children: [
+
+
                 //Logo
                 SvgPicture.asset('assets/images/MyMarca.svg',
                   colorFilter:  ColorFilter.mode(
                       Theme.of(context).colorScheme.primary, BlendMode.srcIn
                   ),
                 ),
+             Text('login',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            ),
+
+                const SizedBox(height: 20,),
+                const MyFormTextField(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  textInputType: TextInputType.emailAddress,
+                  obscureText: false,
+                  suffixIcon: false,
+                ),
+                const SizedBox(height: 20,),
+                const MyFormTextField(
+                    labelText: 'Password',
+                    hintText: 'password',
+                    textInputType: TextInputType.visiblePassword,
+                    obscureText: false,
+                    suffixIcon: true
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 1.0),
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(onPressed: (){},
+                        child: Text('Forgot Password?',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        ),
+                    ),
+                  ],
+
+                )
+                ),
+                const SizedBox(height: 20,),
+                MyButtonForm(
+                    text: 'Login',
+                    onPressed: (){}
+                ),
+
+
+
 
 
 
