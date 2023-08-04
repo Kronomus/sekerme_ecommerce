b57619ee4f3728ae33622da0d20203c9fc2bd4f2
JuanEstebanHernandezRamirez <jehernandez554@soy.sena.edu.co>
2023-08-04 16:40:41 -0500

    Se organizo alg otravez xd

--- lib/app/presentation/views/login/login_view.dart
+++ lib/app/presentation/views/login/login_view.dart
@@ -1,122 +1,149 @@
-
-import 'package:Our_School/app/presentation/views/forgot_password/forgot_%20password_view.dart';
 import 'package:Our_School/app/presentation/views/login/Login_Divider.dart';
+import 'package:Our_School/app/presentation/views/register/register_view.dart';
 import 'package:Our_School/app/presentation/widgets/My_social_button.dart';
 import 'package:Our_School/app/presentation/widgets/form_text_field.dart';
 import 'package:Our_School/app/presentation/widgets/my_button_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
+import 'package:go_router/go_router.dart';
 
 class LoginView extends StatelessWidget {
-
   static const String name = 'login_view';
 
   const LoginView({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment : CrossAxisAlignment.center,
-              children: [
-
-
-                //Logo
-                SvgPicture.asset('assets/images/MyMarca.svg',
-                  colorFilter:  ColorFilter.mode(
-                      Theme.of(context).colorScheme.primary, BlendMode.srcIn
-                  ),
-                  alignment: Alignment.topCenter,
-                  width: 180,
+          child: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //Logo
+              SvgPicture.asset(
+                'assets/images/MyMarca.svg',
+                colorFilter: ColorFilter.mode(
+                    Theme.of(context).colorScheme.primary, BlendMode.srcIn),
+                alignment: Alignment.topCenter,
+                width: 180,
+              ),
+              Text(
+                'login',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
                 ),
-             Text('login',
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            ),
+              ),
 
-                const SizedBox(height: 20,),
-                const MyFormTextField(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  textInputType: TextInputType.emailAddress,
+              const SizedBox(
+                height: 20,
+              ),
+              const MyFormTextField(
+                labelText: 'Email',
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+                obscureText: false,
+                suffixIcon: false,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const MyFormTextField(
+                  labelText: 'Password',
+                  hintText: 'password',
+                  textInputType: TextInputType.visiblePassword,
                   obscureText: false,
-                  suffixIcon: false,
-                ),
-                const SizedBox(height: 20,),
-                const MyFormTextField(
-                    labelText: 'Password',
-                    hintText: 'password',
-                    textInputType: TextInputType.visiblePassword,
-                    obscureText: false,
-                    suffixIcon: true
-                ),
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 1.0),
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                  suffixIcon: true),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 1.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+              MyButtonForm(
+                text: 'Login',
+                onPressed: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+
+              const LoginDivider(),
+              const SizedBox(
+                height: 20,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MySocialButton(
+                    onTap: () {},
+                    imagePath: "assets/icons/google.png",
+                  ),
+                  MySocialButton(
+                    onTap: () {},
+                    imagePath: "assets/icons/facebook.png",
+                  ),
+                  MySocialButton(
+                    onTap: () {},
+                    imagePath: "assets/icons/twitter.png",
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 1.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextButton(onPressed: (){},
-                        child: Text('Forgot Password?',
+                    Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Sign up',
                         style: TextStyle(
                           color: Theme.of(context).colorScheme.primary,
                         ),
-                        ),
+                      ),
                     ),
                   ],
-
-                )
                 ),
-                const SizedBox(height: 20,),
+              ),
 
-                const SizedBox(height: 20,),
-                 MyButtonForm(text: 'Login',
-                     onPressed: () {},
-                ),
-                const SizedBox(height: 10,),
-
-                const LoginDivider(),
-                const SizedBox(height: 20,),
-
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    MySocialButton(
-                        onTap: (){},
-                      imagePath: "assets/icons/google.png",
-                    ),
-                    MySocialButton(
-                      onTap: (){},
-                      imagePath: "assets/icons/facebook.png",
-                    ),
-                    MySocialButton(
-                      onTap: (){},
-                      imagePath: "assets/icons/twitter.png",
-                    ),
-
-                  ],
-                ),
-
-
-
-
-
-                //Form
-                //Button
-                //TextButton
-              ],
-            ),
+              //Form
+              //Button
+              //TextButton
+            ],
           ),
-        )
-      ),
-     );
+        ),
+      )),
+    );
   }
 }
